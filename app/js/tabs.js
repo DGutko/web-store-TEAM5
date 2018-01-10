85d096c0cecbd225df8b8344492ac9ac917d2380
chupilko <chupilko@ukr.net>
2018-01-10 06:15:34 +0200

    tabs + js in tabs.js; -normal (JS) progress-bar

---
+++ app/js/tabs.js
@@ -0,0 +1,16 @@
+let tabsControls = $('[data-tab-controls]');
+
+let tabPane = $('[data-tab-pane]');
+
+tabsControls.on('click', function(e){
+	e.preventDefault(); //отменяем станд действие
+
+	let target = $(e.target).attr('data-tabs'); //переменная, в которой хранится то, что записано в data-tabs
+	tabPane.removeClass('active'); //удаляем класс active
+	tabsControls.removeClass('active'); //удаляем класс active
+
+	$(target).addClass('active'); //добавляем класс active
+
+	$(e.target).addClass('active'); //добавляем класс active
+
+});
